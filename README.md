5e1820a9079203f9ddf0742d4f7ac81f54f18d2b
RamonOliveira95 <ramonsilvadeoliveira95@hotmail.com>
2023-04-28 14:10:08 -0300

    1.0.1

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ A new Flutter project.
 # Aula 1 - 1.0.0
 - Criado o código base do app;
 - Alterado alguns parâmetros do Theme.
+
+# Aula 2 - 1.0.1
+- Criado novo arquivo input_page.dart para criar o design da página principal, com dois containers superiores, um central e dois inferiores;
+- Criado novo Widget ReusableCard() para chamar cada card separadamente;
+- Criado construtor colour para ser adicionado ao ReusableCard(), para poder alterar a cor de cada card.
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,11 +9,62 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
+        appBar: AppBar(
+          title: const Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: const [
+                  Expanded(
+                    child: ReusableCard(colour: Color(0xFF1D1E33)),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      colour: Color(0xFF1D1E33),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Expanded(
+              child: ReusableCard(
+                colour: Color(0xFF1D1E33),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: const [
+                  Expanded(
+                    child: ReusableCard(
+                      colour: Color(0xFF1D1E33),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      colour: Color(0xFF1D1E33),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({super.key, required this.colour});
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
